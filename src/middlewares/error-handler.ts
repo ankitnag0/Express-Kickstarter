cca423a59f9dc2c8dede5038aac891923efc54f3
Ankit Nag <ankitnag08@gmail.com>
2025-02-18 00:20:17 +0530

    remove mongoose error handling

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,7 +1,5 @@
 import { ErrorRequestHandler } from 'express';
-import mongoose from 'mongoose';
 import { CustomError } from '../lib/CustomError';
-import { handleMongooseError } from '../utils/mongoose-error-handler';
 import logger from '../config/logger';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
@@ -19,20 +17,6 @@ const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
   // If it's a known CustomError, return the serialized response
   if (err instanceof CustomError) {
     return res.status(err.status).json(err.serialize());
-  }
-
-  // Handling Mongoose errors (or errors that have a "code" property)
-  if (err instanceof mongoose.Error || 'code' in err) {
-    const mongooseErrorResponse = handleMongooseError(err);
-    return res.status(mongooseErrorResponse.status).json({
-      success: false,
-      status: mongooseErrorResponse.status,
-      error: mongooseErrorResponse.error,
-      message: mongooseErrorResponse.message,
-      ...(mongooseErrorResponse.details && {
-        details: mongooseErrorResponse.details,
-      }),
-    });
   }
 
   // Fallback for unknown errors
