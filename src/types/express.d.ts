6c621f776877ff8ecb52a1329497e5dde4629c1c
Ankit Nag <ankitnag08@gmail.com>
2025-02-19 00:43:46 +0530

    remove redundany type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,13 +3,6 @@ namespace Express {
     user?: { id: string; role: string };
   }
 
-  interface ApiResponse<T = any> {
-    success: boolean;
-    status: number;
-    message: string;
-    data: T;
-  }
-
   interface Response {
     success: <T>(data: T, status?: number, message?: string) => void;
   }
